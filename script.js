feaae6f87fea89d7c499679abb6ec751ad0c6a17
JesusMatuss <skadowasss4@gmail.com>
2026-03-11 11:49:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function mostrarProductos(productos) {
         card.className = 'bg-white p-6 rounded-2xl border border-gray-100 shadow-sm hover:shadow-xl hover:-translate-y-1 transition-all flex flex-col justify-between group';
         card.innerHTML = `
     <div class="relative overflow-hidden rounded-t-2xl">
-        <img src="Logo.png" onerror="this.src='imagenes/placeholder-pan.jpg'" alt="${p.producto}" class="w-full h-40 object-cover group-hover:scale-110 transition-transform duration-500">
+        <img src="Fondo.png" onerror="this.src='imagenes/placeholder-pan.jpg'" alt="${p.producto}" class="w-full h-40 object-cover group-hover:scale-110 transition-transform duration-500">
         <span class="absolute top-2 right-2 bg-white/90 backdrop-blur-sm text-marron-oscuro text-[14px] px-2 py-1 rounded-full font-bold shadow-sm">
             ${p.unidades_pqte} unds
         </span>
@@ -546,5 +546,6 @@ cargarProductos(); // Carga inicial de productos al abrir la página
 
 
 cargarProductos(); // Carga inicial de productos al abrir la página
+
 
 
